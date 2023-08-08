a1e140621d8706f6c774983f89224b723d1f3dc0
betty5634 <g820255@gmail.com>
2023-08-08 20:00:58 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -12,13 +12,7 @@ const Appointment = require("./models/appointmentModel");
 app.use("/api/user", userRoute);
 app.use("/api/doctor", doctorRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
 const port = process.env.PORT || 5000;
 
 
