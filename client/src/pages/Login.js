6e733352781786f48406b75dc0696b8cd831fede
betty5634 <g820255@gmail.com>
2023-08-09 18:31:49 +0530

    update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -16,7 +16,7 @@ function Login() {
   const onFinish = async (values) => {
     try {
       dispatch(showLoading());
-      const response = await axios.get("/api/user/login", values);
+      const response = await axios.post("/api/user/login", values);
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
